ac32d24cc591633c8d2191feee7b4ac390df61ba
unknown <User@AKobetsnout.ensoft.local>
2026-02-21 05:24:10 +0500

    feat: add lab 02 react components and fragments

---
+++ Lab_02/task1/src/App.jsx
@@ -0,0 +1,13 @@
+import FragmentLayout from './FragmentLayout';
+import CombinedList from './CombinedList';
+
+function App() {
+  return (
+    <>
+      <FragmentLayout />
+      <CombinedList />
+    </>
+  );
+}
+
+export default App;
---
+++ Lab_02/task1/src/FragmentLayout.jsx
@@ -0,0 +1,19 @@
+function FragmentLayout() {
+    return(
+        <>
+            <header>
+                <h1>FragmentLayout</h1>
+            </header>
+
+            <main>
+                <p>This layout uses only fragments without extra div wrappers.</p>
+            </main>
+
+            <footer>
+                <small>© 2026 My Project</small> 
+            </footer>
+        </>
+    );
+}
+
+export default FragmentLayout;
